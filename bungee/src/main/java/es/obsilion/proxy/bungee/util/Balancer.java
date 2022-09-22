c90305f4c1f275e83d845b6a8e4e4649ffba18ea
Snake <lluisponsvidal@gmail.com>
2022-09-22 17:12:46 +0200

    Added Beta Player Balancer.

---
+++ bungee/src/main/java/es/obsilion/proxy/bungee/util/Balancer.java
@@ -0,0 +1,33 @@
+package es.obsilion.proxy.bungee.util;
+
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.Server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Balancer {
+    private static final List<String> lobbies = new ArrayList<>();
+
+    static {
+
+        ProxyServer.getInstance().getServersCopy()
+                .forEach((s, serverInfo) -> {
+                    if (s.contains("lobby")) lobbies.add(s);
+                });
+    }
+
+    public static ServerInfo getLessPopulated() {
+        if (lobbies.isEmpty()) return ProxyServer.getInstance().getServerInfo("Lobby");
+
+        ServerInfo lessPopulated = ProxyServer.getInstance().getServerInfo(lobbies.get(0));
+
+        for (int i = 1; i < lobbies.size(); ++i) {
+            if (lessPopulated.getPlayers().size() > ProxyServer.getInstance().getServerInfo(lobbies.get(i)).getPlayers().size())
+                lessPopulated = ProxyServer.getInstance().getServerInfo(lobbies.get(i));
+        }
+
+        return lessPopulated;
+    }
+}
